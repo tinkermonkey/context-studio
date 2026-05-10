1c076ca33ded5dd699278a48c93d684e32b8fd67
Austin Sand <austinsand@gmail.com>
2026-05-10 07:01:56 -0400

    Revert "Remove workspace gate: navigate directly to /app on load"
    This reverts commit 1ebd5d0f52e4701b153d57380158f1183743aa48.

--- ux/src/routes/index.tsx
+++ ux/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { getWorkspacePath } from "@/lib/workspaceStorage";
 
 export const Route = createFileRoute("/")({
   component: IndexPage,
 });
 
 function IndexPage() {
+  const workspacePath = getWorkspacePath();
+
+  if (!workspacePath) {
+    return <Navigate to="/welcome" />;
+  }
+
   return <Navigate to="/app" />;
 }
